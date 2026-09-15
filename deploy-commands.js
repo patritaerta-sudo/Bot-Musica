15dba5eb1c9f61ce75170f29773a918374316ed5
patritaerta-sudo <patritaerta@gmail.com>
2026-09-15 19:35:53 -0300

    Add files via upload

--- deploy-commands.js
+++ deploy-commands.js
@@ -8,19 +8,22 @@ const commands = [
     .addStringOption(option =>
       option.setName('busqueda')
         .setDescription('Nombre correcto de la canción (ej: "Bad Bunny - Monaco")')
-        .setRequired(true))
+        .setRequired(true)),
+  new SlashCommandBuilder()
+    .setName('actualizarhistorial')
+    .setDescription('(Admin) Reescribe todas las fichas viejas del canal con el diseño actual')
 ].map(cmd => cmd.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
   try {
-    console.log('Registrando comando /corregir...');
+    console.log('Registrando comandos /corregir y /actualizarhistorial...');
     await rest.put(
       Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, process.env.DISCORD_GUILD_ID),
       { body: commands }
     );
-    console.log('¡Comando registrado con éxito!');
+    console.log('¡Comandos registrados con éxito!');
   } catch (error) {
     console.error(error);
   }
